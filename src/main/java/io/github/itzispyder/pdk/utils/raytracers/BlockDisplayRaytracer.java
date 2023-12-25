981bff9eff3ec0818929b450c339902415c29e5c
ImproperIssues <ItziSpyder@gmail.com>
2023-12-24 19:17:33 -0800

    fixed the block display raytracer not working on 1.20.1 or lower versions of minecraft

--- src/main/java/io/github/itzispyder/pdk/utils/raytracers/BlockDisplayRaytracer.java
+++ src/main/java/io/github/itzispyder/pdk/utils/raytracers/BlockDisplayRaytracer.java
@@ -8,19 +8,21 @@ import org.bukkit.World;
 import org.bukkit.entity.BlockDisplay;
 import org.bukkit.entity.Display;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.util.Consumer;
 import org.bukkit.util.Transformation;
 import org.bukkit.util.Vector;
 import org.joml.AxisAngle4f;
 import org.joml.Vector3f;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BlockDisplayRaytracer {
 
-    public static void outline(Material display, Location location, long stayTime) {
-        outline(display, location, 0.05, stayTime);
+    public static List<BlockDisplay> outline(Material display, Location location, long stayTime) {
+        return outline(display, location, 0.05, stayTime);
     }
 
-    public static void outline(Material display, Location location, double thickness, long stayTime) {
+    public static List<BlockDisplay> outline(Material display, Location location, double thickness, long stayTime) {
         Location og = location.getBlock().getLocation();
 
         Location a1 = og.clone().add(0, 0, 0);
@@ -33,71 +35,53 @@ public class BlockDisplayRaytracer {
         Location b3 = og.clone().add(1, 1, 1);
         Location b4 = og.clone().add(0, 1, 1);
 
-        trace(display, a1, a2, thickness, stayTime);
-        trace(display, a2, a3, thickness, stayTime);
-        trace(display, a3, a4, thickness, stayTime);
-        trace(display, a4, a1, thickness, stayTime);
+        List<BlockDisplay> a = new ArrayList<>();
 
-        trace(display, b1, b2, thickness, stayTime);
-        trace(display, b2, b3, thickness, stayTime);
-        trace(display, b3, b4, thickness, stayTime);
-        trace(display, b4, b1, thickness, stayTime);
+        a.add(trace(display, a1, a2, thickness, stayTime));
+        a.add(trace(display, a2, a3, thickness, stayTime));
+        a.add(trace(display, a3, a4, thickness, stayTime));
+        a.add(trace(display, a4, a1, thickness, stayTime));
 
-        trace(display, a1, b1, thickness, stayTime);
-        trace(display, a2, b2, thickness, stayTime);
-        trace(display, a3, b3, thickness, stayTime);
-        trace(display, a4, b4, thickness, stayTime);
+        a.add(trace(display, b1, b2, thickness, stayTime));
+        a.add(trace(display, b2, b3, thickness, stayTime));
+        a.add(trace(display, b3, b4, thickness, stayTime));
+        a.add(trace(display, b4, b1, thickness, stayTime));
+
+        a.add(trace(display, a1, b1, thickness, stayTime));
+        a.add(trace(display, a2, b2, thickness, stayTime));
+        a.add(trace(display, a3, b3, thickness, stayTime));
+        a.add(trace(display, a4, b4, thickness, stayTime));
+
+        return a;
     }
 
-    public static void trace(Material display, Location start, Location end, long stayTime) {
-        trace(display, start, end.toVector().subtract(start.toVector()), 0.05, end.distance(start), stayTime);
+    public static BlockDisplay trace(Material display, Location start, Location end, long stayTime) {
+        return trace(display, start, end.toVector().subtract(start.toVector()), 0.05, end.distance(start), stayTime);
     }
 
-    public static void trace(Material display, Location start, Location end, double thickness, long stayTime) {
-        trace(display, start, end.toVector().subtract(start.toVector()), thickness, end.distance(start), stayTime);
+    public static BlockDisplay trace(Material display, Location start, Location end, double thickness, long stayTime) {
+        return trace(display, start, end.toVector().subtract(start.toVector()), thickness, end.distance(start), stayTime);
     }
 
-    public static void trace(Material display, Location start, Vector direction, double thickness, double distance, long stayTime) {
+    public static BlockDisplay trace(Material display, Location start, Vector direction, double thickness, double distance, long stayTime) {
         World world = start.getWorld();
 
-        BlockDisplay beam = world.spawn(start, BlockDisplay.class, entity -> {
-            AxisAngle4f angle = new AxisAngle4f(0, 0, 0, 1);
-            Vector3f transition = new Vector3f(-(float)(thickness / 2F));
-            Vector3f scale = new Vector3f((float)thickness, (float)thickness, (float)distance);
-            Transformation trans = new Transformation(transition, angle, scale, angle);
-            Location vector = entity.getLocation();
+        BlockDisplay entity = world.spawn(start, BlockDisplay.class);
+        AxisAngle4f angle = new AxisAngle4f(0, 0, 0, 1);
+        Vector3f transition = new Vector3f(-(float)(thickness / 2F));
+        Vector3f scale = new Vector3f((float)thickness, (float)thickness, (float)distance);
+        Transformation trans = new Transformation(transition, angle, scale, angle);
+        Location vector = entity.getLocation();
 
-            vector.setDirection(direction);
-            entity.teleport(vector);
-            entity.setBlock(display.createBlockData());
-            entity.setBrightness(new Display.Brightness(15, 15));
-            entity.setInterpolationDelay(0);
-            entity.setTransformation(trans);
+        vector.setDirection(direction);
+        entity.teleport(vector);
+        entity.setBlock(display.createBlockData());
+        entity.setBrightness(new Display.Brightness(15, 15));
+        entity.setInterpolationDelay(0);
+        entity.setTransformation(trans);
 
-            Bukkit.getScheduler().runTaskLater(Global.instance.getPlugin(), entity::remove, stayTime);
-        });
-    }
-
-    public static void trace(Material display, Location start, Vector direction, double thickness, double distance, long stayTime, Consumer<BlockDisplay> onEntitySpawn) {
-        World world = start.getWorld();
-
-        BlockDisplay beam = world.spawn(start, BlockDisplay.class, entity -> {
-            AxisAngle4f angle = new AxisAngle4f(0, 0, 0, 1);
-            Vector3f transition = new Vector3f(-(float)(thickness / 2F));
-            Vector3f scale = new Vector3f((float)thickness, (float)thickness, (float)distance);
-            Transformation trans = new Transformation(transition, angle, scale, angle);
-            Location vector = entity.getLocation();
-
-            vector.setDirection(direction);
-            entity.teleport(vector);
-            entity.setBlock(display.createBlockData());
-            entity.setBrightness(new Display.Brightness(15, 15));
-            entity.setInterpolationDelay(0);
-            entity.setTransformation(trans);
-
-            JavaPlugin plugin = Global.instance.getPlugin();
-            Bukkit.getScheduler().runTaskLater(plugin, entity::remove, stayTime);
-            Bukkit.getScheduler().runTaskLater(plugin, () -> onEntitySpawn.accept(entity), 5);
-        });
+        JavaPlugin plugin = Global.instance.getPlugin();
+        Bukkit.getScheduler().runTaskLater(plugin, entity::remove, stayTime);
+        return entity;
     }
 }
